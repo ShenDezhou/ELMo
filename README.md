1aef7594ca9dcbdb1c9555d137a25fabbebd4e35
ShenDezhou <tsinghua9boy@sina.com>
2022-05-31 19:44:34 +0800

    1. cn ELMo.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Caution
+Too slow to run the training on cpu.
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,3 @@
-python run_training.py --train_data_path='../data_processed/*' --vocab_file=../cn_corpus/comment_vocab.txt --save_dir='./trained_model' --device=cpu --max_characters_per_token=50 --projection_dim=300
+python run_training.py --train_data_path='../data_processed/*' --vocab_file=../cn_corpus/comment_vocab.txt --save_dir='./trained_model' --device=cpu \
+ --max_characters_per_token=20 --projection_dim=32 \
+ --batch_size=16 --num_layers=1 --num_highways=1
